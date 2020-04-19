5be0582bd0db88f3b41f86f362c9467d3917ac64
ttnny <ttnny@users.noreply.github.com>
2020-04-18 22:22:52 -0700

    Update

--- threading-fundamentals/src/Main.java
+++ threading-fundamentals/src/Main.java
@@ -1,16 +1,110 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        Thread thread = new Thread(() -> {
-            // Code that will run in a new thread
-            System.out.println(Thread.currentThread().getId() + ", " + Thread.currentThread().getName() + ", " + Thread.currentThread().getPriority());
-        });
+    public static final int MAX_PASSWORD = 9999;
 
-        thread.setName("Earth Thread");
-        thread.setPriority(Thread.MAX_PRIORITY);
-        thread.start();
+    public static void main(String[] args) {
+        Random random = new Random();
 
-        System.out.println(Thread.currentThread().getId() + ", " + Thread.currentThread().getName() + ", " + Thread.currentThread().getPriority());
+        Vault vault = new Vault(random.nextInt(MAX_PASSWORD));
+        System.out.println("Vault's password: " + vault.password);
+        System.out.println();
 
-        Thread.sleep(3000);
+        List<Thread> threads = new ArrayList<>();
+
+        threads.add(new AscendingHackerThread(vault));
+        threads.add(new DescendingHackerThread(vault));
+        threads.add(new PoliceThread());
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+    }
+
+    private static class Vault {
+        private final int password;
+
+        public Vault(int password) {
+            this.password = password;
+        }
+
+        public boolean isCorrectPassword(int guess) {
+            try {
+                Thread.sleep(3);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return this.password == guess;
+        }
+    }
+
+    private static abstract class HackerThread extends Thread {
+        protected Vault vault;
+
+        public HackerThread(Vault vault) {
+            this.vault = vault;
+            this.setName(this.getClass().getSimpleName());
+            this.setPriority(Thread.MAX_PRIORITY);
+        }
+
+        @Override
+        public void start() {
+            System.out.println("Starting thread " + this.getName());
+            super.start();
+        }
+    }
+
+    private static class AscendingHackerThread extends HackerThread {
+        public AscendingHackerThread(Vault vault) {
+            super(vault);
+        }
+
+        @Override
+        public void run() {
+            for (int guess = 0; guess < MAX_PASSWORD; guess++) {
+                if (vault.isCorrectPassword(guess)) {
+                    System.out.println(this.getName() + " guessed the password: " + guess);
+                    System.exit(0);
+                }
+            }
+        }
+    }
+
+    private static class DescendingHackerThread extends HackerThread {
+        public DescendingHackerThread(Vault vault) {
+            super(vault);
+        }
+
+        @Override
+        public void run() {
+            for (int guess = MAX_PASSWORD; guess >= 0; guess--) {
+                if (vault.isCorrectPassword(guess)) {
+                    System.out.println(this.getName() + " guessed the password: " + guess);
+                    System.exit(0);
+                }
+            }
+        }
+    }
+
+    private static class PoliceThread extends Thread {
+        @Override
+        public void run() {
+            this.setName(this.getClass().getSimpleName());
+            System.out.println("Starting thread " + this.getName());
+
+            for (int i = 10; i > 0; i--) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(i);
+            }
+
+            System.out.println("Game over, hackers!");
+            System.exit(0);
+        }
     }
 }
