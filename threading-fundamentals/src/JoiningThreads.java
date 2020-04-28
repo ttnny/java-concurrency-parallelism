64ac0d246e1bc842f2e76743d2b7a49399453bd8
ttnny <ttnny@users.noreply.github.com>
2020-04-27 23:29:05 -0700

    Update

--- threading-fundamentals/src/JoiningThreads.java
+++ threading-fundamentals/src/JoiningThreads.java
@@ -1,4 +1,5 @@
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,7 +8,24 @@ public class JoiningThreads {
         List<Long> inputNumbers = Arrays.asList(0L, 3456L, 23456L, 4005L, 5790L, 89L, 2020L, 6996L);
         // Calculate 0!, 3456!, 23456!, 4005!, 5790!, 89!, 2020!, 6996!
 
+        List<FactorialThread> threads = new ArrayList<>();
 
+        for (long inputNumber : inputNumbers) {
+            threads.add(new FactorialThread(inputNumber));
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+        }
+
+        for (int i = 0; i < inputNumbers.size(); i++) {
+            FactorialThread factorialThread = threads.get(i);
+            if (factorialThread.isFinished) {
+                System.out.println(inputNumbers.get(i) + " is " + factorialThread.getResult());
+            } else {
+                System.out.println(inputNumbers.get(i) + " is still in progress");
+            }
+        }
     }
 
     public static class FactorialThread extends Thread {
@@ -21,7 +39,26 @@ public class JoiningThreads {
 
         @Override
         public void run() {
-            super.run();
+            this.result = factorial(inputNumber);
+            this.isFinished = true;
+        }
+
+        public BigInteger factorial(long n) {
+            BigInteger tempResult = BigInteger.ONE;
+
+            for (long i = n; i > 0; i--) {
+                tempResult = tempResult.multiply(new BigInteger(Long.toString(i)));
+            }
+
+            return tempResult;
+        }
+
+        public boolean isFinished() {
+            return isFinished;
+        }
+
+        public BigInteger getResult() {
+            return result;
         }
     }
 }
